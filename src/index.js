7e8d191555129c37d5990fa8f9b4e90ef9f3fb78
Bruno Girome <brunogirome@gmail.com>
2020-04-18 23:48:21 -0300

    Configurações básicas do SEQUELIZE

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import express from 'express';
+import 'dotenv/config';
 
-const app = express();
+import app from './app';
 
-app.use(express.Router());
-app.use(express.json());
-
-app.get('/', (req, res) => res.json({ message: 'Hello World.' }));
-
-app.listen(3333);
+app.listen(process.env.APP_PORT);
