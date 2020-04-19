36295142bf37ae5abc464484db1aede2294f70c3
Bruno Girome <brunogirome@gmail.com>
2020-04-19 15:12:38 -0300

    Criação do Model de Endereços

---
+++ src/app/models/Endereco.js
@@ -0,0 +1,36 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Endereco extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        rua: Sequelize.STRING,
+        numero: Sequelize.INTEGER,
+        bairro: Sequelize.STRING,
+        cidade: Sequelize.STRING,
+        estado: Sequelize.STRING,
+        cep: Sequelize.STRING,
+        observacoes: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Usuario, {
+      foreignKey: 'id_usuario',
+      as: 'usuario',
+    });
+
+    this.belongsTo(models.Pedido, {
+      foreignKey: 'id_endereco',
+      as: 'endereco',
+    });
+  }
+}
+
+export default Endereco;
